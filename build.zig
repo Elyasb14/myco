ef279f475e26777f9240c5927b9c1bbc23b1d173
Elyas Bianchi <elyasb99@gmail.com>
2025-11-17 20:07:07 +0000

    add some testing

--- build.zig
+++ build.zig
@@ -13,6 +13,17 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
+    const netlink_mod = b.addModule("netlink", .{
+        .root_source_file = b.path("src/netlink/netlink.zig"),
+    });
+
+    myco_exe.root_module.addImport("netlink", netlink_mod);
     myco_exe.linkLibC();
     b.installArtifact(myco_exe);
+
+    const test_add_delete = b.addExecutable(.{ .name = "test_add_delete", .root_module = b.createModule(.{ .root_source_file = b.path("test/add_delete.zig"), .target = target, .optimize = optimize }) });
+    test_add_delete.root_module.addImport("netlink", netlink_mod);
+    test_add_delete.linkLibC();
+
+    b.installArtifact(test_add_delete);
 }
